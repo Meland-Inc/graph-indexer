87b960b6539e655200ff4637a1062ceb5de6a18f
Albert <r.albert.huang@gmail.com>
2021-11-24 18:16:51 +0800

    chore: 添加NFTSupport查询

--- src/maps/NFTFactory.ts
+++ src/maps/NFTFactory.ts
@@ -1,6 +1,11 @@
 import { NFTSupportCreate } from '../generated/entities/NFTFactory/NFTFactory';
 import { NFTWithRarity as ERC721 } from '../generated/entities/templates';
+import { NFTSupport } from '../generated/entities/schema';
 
 export function handleNFTSupportCreate(event: NFTSupportCreate): void {
 	ERC721.create(event.params.nft);
+
+	let snft = new NFTSupport(event.params.nft.toHex());
+	snft.nftAddress = event.params.nft;
+	snft.save();
 }
