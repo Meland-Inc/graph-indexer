86376f80ac54abbb5a4d7d9bb08b9abe37046cd5
Albert <r.albert.huang@gmail.com>
2021-12-02 14:34:04 +0800

    rename

--- src/log.ts
+++ src/log.ts
@@ -1,16 +1,16 @@
-import { CreateorderLog, Log, BoughtLog, TransferLog, NFT, Order, CancelorderLog } from './generated/entities/schema';
+import { CreateorderLog, Web3Log, BoughtLog, TransferLog, NFT, Order, CancelorderLog } from './generated/entities/schema';
 import { ethereum } from '@graphprotocol/graph-ts/chain/ethereum';
 import { BigInt } from '@graphprotocol/graph-ts';
 import { buildAccount } from './account';
 import { LogAction_bought, LogAction_cancelorder, LogAction_createorder, LogAction_transfer, LogAction_updateorder } from './enums';
 
-export function createorderLog(event: ethereum.Event, order: Order): Log {
+export function createorderLog(event: ethereum.Event, order: Order): Web3Log {
     let logId = event.transaction.hash.toHex();
 	let oLog = new CancelorderLog(logId);
     oLog.order = order.id;
     oLog.save();
 
-	let log = new Log(logId);
+	let log = new Web3Log(logId);
 	log.account = buildAccount(event.transaction.from).id;
 	log.action = LogAction_createorder;
 	log.bought = oLog.id;
@@ -20,13 +20,13 @@ export function createorderLog(event: ethereum.Event, order: Order): Log {
 	return log;
 }
 
-export function updateorderLog(event: ethereum.Event, order: Order): Log {
+export function updateorderLog(event: ethereum.Event, order: Order): Web3Log {
     let logId = event.transaction.hash.toHex();
 	let oLog = new CancelorderLog(logId);
     oLog.order = order.id;
     oLog.save();
 
-	let log = new Log(logId);
+	let log = new Web3Log(logId);
 	log.account = buildAccount(event.transaction.from).id;
 	log.action = LogAction_updateorder;
 	log.bought = oLog.id;
@@ -36,13 +36,13 @@ export function updateorderLog(event: ethereum.Event, order: Order): Log {
 	return log;
 }
 
-export function cancelorderLog(event: ethereum.Event, order: Order): Log {
+export function cancelorderLog(event: ethereum.Event, order: Order): Web3Log {
     let logId = event.transaction.hash.toHex();
 	let oLog = new CancelorderLog(logId);
     oLog.order = order.id;
     oLog.save();
 
-	let log = new Log(logId);
+	let log = new Web3Log(logId);
 	log.account = buildAccount(event.transaction.from).id;
 	log.action = LogAction_cancelorder;
 	log.bought = oLog.id;
@@ -52,14 +52,14 @@ export function cancelorderLog(event: ethereum.Event, order: Order): Log {
 	return log;
 }
 
-export function transferLog(event: ethereum.Event, nft: NFT): Log {
+export function transferLog(event: ethereum.Event, nft: NFT): Web3Log {
     let logId = event.transaction.hash.toHex();
 	let transferLog = new TransferLog(logId);
     transferLog.to = buildAccount(event.transaction.to!).id;
     transferLog.nft = nft.id;
     transferLog.save();
 
-	let log = new Log(logId);
+	let log = new Web3Log(logId);
 	log.account = buildAccount(event.transaction.from).id;
 	log.action = LogAction_transfer;
 	log.bought = transferLog.id;
@@ -69,14 +69,14 @@ export function transferLog(event: ethereum.Event, nft: NFT): Log {
 	return log;
 }
 
-export function boughtLog(event: ethereum.Event, nft: NFT, priceInWei: BigInt): Log {
+export function boughtLog(event: ethereum.Event, nft: NFT, priceInWei: BigInt): Web3Log {
 	let logId = event.transaction.hash.toHex();
 	let boughtLog = new BoughtLog(logId);
 	boughtLog.nft = nft.id;
 	boughtLog.priceInWei = priceInWei;
 	boughtLog.save();
 
-	let log = new Log(logId);
+	let log = new Web3Log(logId);
 	log.account = buildAccount(event.transaction.from).id;
 	log.action = LogAction_bought;
 	log.bought = boughtLog.id;
