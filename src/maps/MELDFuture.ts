f6bd55be8f7720d061ad720f975ea5e40939a622
AlbertHuang <r.albert.huang@gmail.com>
2022-01-09 19:31:05 +0800

    fix: graph ts

--- src/maps/MELDFuture.ts
+++ src/maps/MELDFuture.ts
@@ -3,7 +3,7 @@ import { NFTProtocol_erc1155 } from '../enums';
 import { TransferSingle, TransferBatch } from '../generated/entities/templates/ERC1155/ERC1155';
 import { buildNFT, gensHandleTransfer } from '../nft';
 import { Claim, Meland1155MELDFuture } from '../generated/entities/MELDFuture/Meland1155MELDFuture';
-import { FutureAmount, FutureClaim } from '../generated/entities/schema';
+import { FutureAmount, FutureClaim, _FutureClaimMeta_ } from '../generated/entities/schema';
 import { Address, BigInt } from '@graphprotocol/graph-ts';
 
 export function handleTransferSingle(event: TransferSingle): void {
@@ -41,6 +41,18 @@ function buildFutureAmount(beneficiary: Address): FutureAmount {
         fa.save();
     }
     return fa;
+}
+
+function buildFutureClaimMeta(beneficiary: Address): _FutureClaimMeta_ {
+    let metaId = beneficiary.toHex();
+    let meta = _FutureClaimMeta_.load(metaId);
+    if (meta == null) {
+        meta = new _FutureClaimMeta_(metaId);
+        meta.beneficiary = beneficiary;
+        meta.count = BigInt.fromI32(0);
+        meta.save();
+    }
+    return meta;
 }
 
 function handFromTransfer(
@@ -83,6 +95,9 @@ export function handleClaim(event: Claim): void {
     fc.beneficiary = event.params.beneficiary;
     fc.txHash = event.transaction.hash;
     fc.save();
+    let claimMeta = buildFutureClaimMeta(event.params.beneficiary)
+    claimMeta.count = claimMeta.count.plus(BigInt.fromI32(1));
+    claimMeta.save();
 }
 
 export function handleTransferBatch(event: TransferBatch): void {
